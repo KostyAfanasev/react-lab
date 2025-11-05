b509dd8505b7abe7b805183ca097dd083a3cd5c3
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-06 00:13:01 +0500

    2_7_3

--- src/2/2_7_3/App.tsx
+++ src/2/2_7_3/App.tsx
@@ -1,8 +1,3 @@
-// 2_7_3 Fix the mutations using non-mutative methods  
-/*
-    В этом примере все обработчики событий в App.js используют мутацию. В результате редактирование и удаление todos не работает. Перепишите handleAddTodo, handleChangeTodo и handleDeleteTodo, чтобы они использовали немутационные методы:
-*/
-
 import { useState } from 'react';
 import AddTodo from './AddTodo';
 import TaskList from './TaskList';
@@ -24,26 +19,28 @@ export default function TaskApp() {
     const [todos, setTodos] = useState(initialTodos);
 
     function handleAddTodo(title: string) {
-        todos.push({
-            id: nextId++,
-            title: title,
-            done: false,
-        });
+        setTodos([
+            ...todos,
+            {
+                id: nextId++,
+                title: title,
+                done: false,
+            }
+        ]);
     }
 
     function handleChangeTodo(nextTodo: Todo) {
-        const todo = todos.find(
-            (t) => t.id === nextTodo.id
-        )!!;
-        todo.title = nextTodo.title;
-        todo.done = nextTodo.done;
+        setTodos(todos.map(todo =>
+            todo.id === nextTodo.id
+                ? nextTodo
+                : todo
+        ));
     }
 
     function handleDeleteTodo(todoId: number) {
-        const index = todos.findIndex(
-            (t) => t.id === todoId
+        setTodos(
+            todos.filter(t => t.id !== todoId)
         );
-        todos.splice(index, 1);
     }
 
     return (
