a73b0ba2836516a16bb832848caa4f4875034c92
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-05 15:04:35 +0500

    1_5_2

--- src/1/1_5_2/App.tsx
+++ src/1/1_5_2/App.tsx
@@ -1,10 +1,3 @@
-// 1_5_2 Adjust the image size based on a prop 
-/*
-  В этом примере Avatar получает числовой параметр size, который определяет ширину и высоту <img>. В данном примере параметр size установлен на 40. Однако если вы откроете изображение в новой вкладке, вы заметите, что само изображение больше (160 пикселей). Реальный размер изображения определяется тем, какой размер миниатюры вы запрашиваете.
-
-  Измените компонент Avatar, чтобы он запрашивал наиболее близкий размер изображения на основе параметра size. В частности, если size меньше 90, передавайте 's' ("small"), а не 'b' ("big") в функцию getImageUrl. Проверьте, что ваши изменения работают, отобразив аватары с разными значениями параметра size и открыв изображения в новой вкладке.
-*/
-
 import { getImageUrl } from "./util";
 
 export type Person = {
@@ -12,11 +5,12 @@ export type Person = {
   imageId: string;
 };
 
+// Изменен компонент Avatar, чтобы он передавал 's' или 'b' в зависимости от значения size
 function Avatar({ person, size }: { person: Person; size: number }) {
   return (
     <img
       className="avatar"
-      src={getImageUrl(person, "b")}
+      src={getImageUrl(person, size < 90 ? "s" : "b")}
       alt={person.name}
       width={size}
       height={size}
