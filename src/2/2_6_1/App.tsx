9d1b9143c72a3b46799549f790e1130c40ce4031
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-05 23:00:10 +0500

    2_6_1

--- src/2/2_6_1/App.tsx
+++ src/2/2_6_1/App.tsx
@@ -1,10 +1,3 @@
-// 2_6_1 Fix incorrect state updates 
-/*
-    В этой форме есть несколько ошибок. Несколько раз нажмите на кнопку, увеличивающую оценку. Заметьте, что он не увеличивается. Затем отредактируйте имя и фамилию и заметите, что оценка внезапно "подхватила" ваши изменения. Наконец, отредактируйте фамилию, и заметите, что оценка полностью исчезла.
-
-    Ваша задача — исправить все эти ошибки. Исправляя их, объясните, почему происходит каждая из них.
-*/
-
 import { useState } from 'react';
 
 export default function Scoreboard() {
@@ -15,7 +8,10 @@ export default function Scoreboard() {
     });
 
     function handlePlusClick() {
-        player.score++;
+        setPlayer({
+            ...player,
+            score: player.score + 1,
+        });
     }
 
     function handleFirstNameChange(e: any) {
@@ -27,8 +23,9 @@ export default function Scoreboard() {
 
     function handleLastNameChange(e: any) {
         setPlayer({
+            ...player,
             lastName: e.target.value,
-        } as any);
+        });
     }
 
     return (
