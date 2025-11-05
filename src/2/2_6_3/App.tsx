254579e314817f2ec6827d9307178bf4e0a651d0
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-05 23:24:23 +0500

    2_6_3

--- src/2/2_6_3/App.tsx
+++ src/2/2_6_3/App.tsx
@@ -1,13 +1,3 @@
-// 2_6_3 Update an object with Immer 
-/*
-    Имеется перетаскиваемый ящик на статичном фоне. Вы можете изменить цвет поля с помощью кнопки select.
-
-    Но есть ошибка. Если сначала переместить ящик, а затем изменить его цвет, фон (который не должен двигаться!) "перепрыгнет" на позицию ящика. Но этого не должно произойти: параметр position у Background установлен в initialPosition, что равно { x: 0, y: 0 }. Почему фон перемещается после изменения цвета?
-
-    На этот раз исправьте мутацию, используя Immer. Для вашего удобства функция useImmer уже импортирована, поэтому вам нужно изменить переменную состояния shape, чтобы использовать ее.
-*/
-
-import { useState } from 'react';
 import { useImmer } from 'use-immer';
 import Background from './Background';
 import Box from './Box';
@@ -20,20 +10,21 @@ const initialPosition = {
 };
 
 export default function Canvas() {
-    const [shape, setShape] = useState({
+    const [shape, setShape] = useImmer({
         color: 'orange',
         position: initialPosition,
     });
 
     function handleMove(dx: number, dy: number) {
-        shape.position.x += dx;
-        shape.position.y += dy;
+        setShape(draft => {
+            draft.position.x += dx;
+            draft.position.y += dy;
+        });
     }
 
     function handleColorChange(e: any) {
-        setShape({
-            ...shape,
-            color: e.target.value,
+        setShape(draft => {
+            draft.color = e.target.value;
         });
     }
 
