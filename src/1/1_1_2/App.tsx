ae7b6ca3a9e076dee92b83615fe5e562737ab8ad
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-05 11:54:55 +0500

    1_1_2

--- src/1/1_1_2/App.tsx
+++ src/1/1_1_2/App.tsx
@@ -1,11 +1,6 @@
-// 1_1_2 Fix the return statement 
-/* 
-  Исправьте ошибку в операторе return
-*/
-
+// Компонент Profile исправлен: возвращаемое значение теперь на той же строке, что и оператор return
 export default function Profile() {
-    return
-      <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />;
-  }
-  
-  
+  return (
+    <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />
+  );
+}
