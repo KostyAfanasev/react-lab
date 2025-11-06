cacd83d8c168fdb07ef33e54e11be4599c35a012
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-06 18:52:00 +0500

    3_2_3

--- src/3/3_2_3/App.tsx
+++ src/3/3_2_3/App.tsx
@@ -1,11 +1,3 @@
-// 3_2_3 Fix the disappearing selection 
-/*
-    Выводится список писем, которые хранятся в переменной состояния letters. Когда вы наводите курсор или фокус на определенное письмо - оно выделяется (подсвечивается). Текущее выделенное письмо хранится в переменной состояния highlightedLetter. Вы можете "выделять" и "снимать выделение" отдельных писем, что приводит к обновлению массива letters в состоянии.
-
-    Этот код работает, но есть небольшой сбой в пользовательском интерфейсе. Когда вы нажимаете "Star" или "Unstar", подсветка на мгновение исчезает. Однако она снова появляется, как только вы перемещаете указатель или переключаетесь на другое письмо с клавиатуры. Почему это происходит? Исправьте это, чтобы подсветка не исчезала после нажатия кнопки.
-*/
-
-
 import { useState } from 'react';
 import { initialLetters, LetterType } from './data.js';
 import Letter from './Letter.js';
@@ -41,7 +33,7 @@ export default function MailClient() {
                         key={letter.id}
                         letter={letter}
                         isHighlighted={
-                            letter === highlightedLetter
+                            letter.id === highlightedLetter?.id
                         }
                         onHover={handleHover}
                         onToggleStar={handleStar}
