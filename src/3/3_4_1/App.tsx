f1fd4c4b281513f9ac9f0b32be41672081dafdcb
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-06 20:29:02 +0500

    3_4_1

--- src/3/3_4_1/App.tsx
+++ src/3/3_4_1/App.tsx
@@ -1,29 +1,16 @@
-// 3_4_1 Fix disappearing input text 
-/*
-  Этот пример показывает сообщение при нажатии на кнопку. Однако при нажатии кнопки также происходит случайный сброс ввода. Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
-*/
-
 import { useState } from 'react';
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
-        <button onClick={() => {
-          setShowHint(false);
-        }}>Hide hint</button>
-      </div>
-    );
-  }
   return (
     <div>
+      {showHint && <p><i>Hint: Your favorite city?</i></p>}
       <Form />
       <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
+        setShowHint(!showHint);
+      }}>
+        {showHint ? 'Hide hint' : 'Show hint'}
+      </button>
     </div>
   );
 }
