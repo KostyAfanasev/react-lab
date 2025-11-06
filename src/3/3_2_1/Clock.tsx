01b0f7845613ac03c2d9fdf934de9fbc0fab9ca1
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-06 18:25:35 +0500

    3_2_1

--- src/3/3_2_1/Clock.tsx
+++ src/3/3_2_1/Clock.tsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Clock(props: { time: string, color: string }) {
   const [color, setColor] = useState(props.color);
+
+  useEffect(() => {
+    setColor(props.color);
+  }, [props.color]);
+
   return (
     <h1 style={{ color: color }}>
       {props.time}
