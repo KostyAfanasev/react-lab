078bc5dccad87803f7a9eb4f67b30315409ab66f
KostyAfanasev <kostyafanasev@mail.ru>
2025-11-07 22:30:27 +0500

    4_8_3

--- src/4/4_8_3/useCounter.ts
+++ src/4/4_8_3/useCounter.ts
@@ -1,20 +1,12 @@
-import { useState, useEffect } from 'react';
+// Извлечен хук useInterval в отдельный файл для управления интервалами
+
+import { useState } from 'react';
+import { useInterval } from './useInterval';
 
 export function useCounter(delay: number = 1000) {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCount(c => c + 1);
-    }, delay);
-    return () => clearInterval(id);
-  }, [delay]);
+  useInterval(() => {
+    setCount(c => c + 1);
+  }, delay);
   return count;
 }
-// Новая версия
-// export function useCounter(delay) {
-//     const [count, setCount] = useState(0);
-//     useInterval(() => {
-//         setCount((c) => c + 1);
-//     }, delay);
-//     return count;
-//   }
